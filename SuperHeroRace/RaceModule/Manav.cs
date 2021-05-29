8d8715020ad1c5a4f6c3fa67e1d441803350732f
Manav <Manav@gmail.com>
2021-05-29 20:40:19 +1200

    Add project files.

---
+++ SuperHeroRace/RaceModule/Manav.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeroRace.RaceModule
+{
+    public class Manav : Punter
+    {
+        public Manav()
+        {
+            this.Name = "Manav";
+        }
+    }
+}
